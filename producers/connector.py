f15dfebe095207ce9667ab7c9f47acece1067100
Tianchi Zhang <ti.zhang@fetchrewards.com>
2024-09-02 16:20:10 -0400

    [FEATURE-STEP1] set up producer code: - producer.py - arrival, turnstile, weather -- key and value avro schemas - station.py - turnstile.py - weather.py - connector.py

---
+++ producers/connector.py
@@ -0,0 +1,66 @@
+"""Configures a Kafka Connector for Postgres Station data"""
+import json
+import logging
+
+import requests
+
+
+logger = logging.getLogger(__name__)
+
+
+KAFKA_CONNECT_URL = "http://localhost:8083/connectors"
+# KAFKA_CONNECT_URL = "http://kafka-connect:8083/connectors"
+CONNECTOR_NAME = "stations-jdbc"
+
+def configure_connector():
+    """Starts and configures the Kafka Connect connector"""
+    
+    logging.debug("creating or updating kafka connect connector...")
+
+    resp = requests.get(f"{KAFKA_CONNECT_URL}/{CONNECTOR_NAME}")
+    if resp.status_code == 200:
+        logging.debug("connector already created skipping recreation")
+        return
+
+    # TODO (done): Complete the Kafka Connect Config below.
+    # Directions: Use the JDBC Source Connector to connect to Postgres. Load the `stations` table
+    # using incrementing mode, with `stop_id` as the incrementing column name.
+    # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
+    # Connect should run this connector (hint: not very often!)
+
+    # Define the connector configuration
+    connector_config = {
+        "name": CONNECTOR_NAME,
+        "config": {
+            "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+            "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+            "key.converter.schemas.enable": "false",
+            "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+            "value.converter.schemas.enable": "false",
+            "batch.max.rows": "500",  # Maximum number of rows to include in a single batch
+            "connection.url": "jdbc:postgresql://postgres:5432/cta",  # Connection URL for the Postgres database (Docker)
+            # "connection.url": "jdbc:postgresql://localhost:5432/cta",  # Connection URL for the Postgres database (Local host)
+            "connection.user": "cta_admin",  # Postgres username
+            "connection.password": "chicago",  # Postgres password
+            "table.whitelist": "stations",  # Only capture data from the `stations` table
+            "mode": "incrementing",  # Use incrementing mode for capturing changes
+            "incrementing.column.name": "stop_id",  # Use `stop_id` as the incrementing column
+            "topic.prefix": "cta.",  # Prefix for Kafka topics, resulting in `cta_stations_stations`
+            "poll.interval.ms": "3600000",  # Poll the database every hour (3600000 milliseconds)
+        }
+    }
+
+    # Post the configuration to Kafka Connect
+    resp = requests.post(
+        KAFKA_CONNECT_URL,
+        headers={"Content-Type": "application/json"},
+        data=json.dumps(connector_config)
+    )
+
+    # Ensure a healthy response was given
+    resp.raise_for_status()
+    logging.debug("connector created successfully")
+
+
+if __name__ == "__main__":
+    configure_connector()
---
+++ producers/models/producer.py
@@ -0,0 +1,100 @@
+"""Producer base-class providing common utilites and functionality"""
+import logging
+import time
+
+
+from confluent_kafka import avro
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka.avro import AvroProducer
+
+logger = logging.getLogger(__name__)
+
+
+class Producer:
+    """Defines and provides common functionality amongst Producers"""
+
+    # Tracks existing topics across all Producer instances
+    existing_topics = set([])
+
+    def __init__(
+        self,
+        topic_name,
+        key_schema,
+        value_schema=None,
+        num_partitions=1,
+        num_replicas=1,
+    ):
+        """Initializes a Producer object with basic settings"""
+        self.topic_name = topic_name
+        self.key_schema = key_schema
+        self.value_schema = value_schema
+        self.num_partitions = num_partitions
+        self.num_replicas = num_replicas
+
+        #
+        #
+        # TODO (done): Configure the broker properties below. Make sure to reference the project README
+        # and use the Host URL for Kafka and Schema Registry!
+        #
+        #
+        self.broker_properties = {
+            # TODO (done)
+            "bootstrap.servers": "PLAINTEXT://localhost:9092", # Kafka broker address
+            # TODO (done)
+            "schema.registry.url": "http://localhost:8081", # Schema Registry URL
+            # "schema.registry.url": "http://schema-registry:8081", # Schema Registry URL
+            # TODO
+        }
+
+        # If the topic does not already exist, try to create it
+        if self.topic_name not in Producer.existing_topics:
+            self.create_topic()
+            Producer.existing_topics.add(self.topic_name)
+
+        # TODO (done): Configure the AvroProducer
+        self.producer = AvroProducer(
+            self.broker_properties,
+            default_key_schema=self.key_schema,
+            default_value_schema=self.value_schema,
+        )
+
+    def create_topic(self):
+        """Creates the producer topic if it does not already exist"""
+        #
+        #
+        # TODO (done): Write code that creates the topic for this producer if it does not already exist on
+        # the Kafka Broker.
+        #
+        #
+        client = AdminClient({"bootstrap.servers": self.broker_properties["bootstrap.servers"]})
+        
+        topic_metadata = client.list_topics(timeout=5)
+        if self.topic_name not in set(t.topic for t in iter(topic_metadata.topics.values())):
+            topic = NewTopic(
+                topic=self.topic_name,
+                num_partitions=self.num_partitions,
+                replication_factor=self.num_replicas,
+            )
+            client.create_topics([topic])
+            logger.info(f"Topic {self.topic_name} created successfully.")
+        else:
+            logger.info(f"Topic {self.topic_name} already exists.")
+
+        # logger.info("topic creation kafka integration incomplete - skipping")
+
+    def close(self):
+        """Prepares the producer for exit by cleaning up the producer"""
+        #
+        #
+        # TODO: Write cleanup code for the Producer here
+        #
+        #
+        if self.producer is not None:
+            self.producer.flush()
+            logger.info("Producer flushed and closed successfully.")
+
+        # logger.info("producer close incomplete - skipping")
+
+    def time_millis(self):
+        """Use this function to get the key for Kafka Events"""
+        return int(round(time.time() * 1000))
